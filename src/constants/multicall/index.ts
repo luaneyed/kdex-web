dc6f4168d71cddce9fd152ad69b250793b08eb41
Sangguk Lee <sangguk258@gmail.com>
2021-10-20 14:54:15 +0900

    Use my multicall on baobab

--- src/constants/multicall/index.ts
+++ src/constants/multicall/index.ts
@@ -3,7 +3,7 @@ import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
   [ChainId.CYPRESS]: '0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb', // TODO
-  [ChainId.BAOBAB]: '0x301907b5835a2d723Fe3e9E8C5Bc5375d5c1236A'
+  [ChainId.BAOBAB]: '0x9f405a0a22a2158345cda22cc1881579b1add85e'
 }
 
 export { MULTICALL_ABI, MULTICALL_NETWORKS }
